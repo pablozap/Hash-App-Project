b7d2382e727a103225055a7a63e7375d38b5c190
Pablo Z <juan.zapata046@pascualbravo.edu.co>
2024-04-21 17:21:35 -0500

    Gradle updated from 8.2.0 to 8.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.3.2" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
